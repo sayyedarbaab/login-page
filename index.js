71fba9d5a75048ff3b74fd0f2c40e6c0464257c3
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-01-26 17:22:36 +0530

    add commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function showModel(){
+    document.querySelector('.overlay').classList.add('showoverlay');
+    document.querySelector('.loginform').classList.add('showloginform');
+}
+function closeModel(){
+    document.querySelector('.overlay').classList.remove('showoverlay');
+    document.querySelector('.loginform').classList.remove('showloginform');
+}
+
+var btnClick=document.querySelector('.btn-login');
+btnClick.addEventListener("click",showModel)
+
+
+var close=document.querySelector('span');
+close.addEventListener("click",closeModel)
+
+var closeByOverlay=document.querySelector('.overlay')
+closeByOverlay.addEventListener('click',closeModel)
